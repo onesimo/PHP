038ad033956b876432488c9a59fb0cd6c81f85b3
Onesimo Batista <onesimobatista@gmail.com>
2016-09-21 19:23:22 -0300

    first commit

--- news_php7.php
+++ news_php7.php
@@ -1,1 +1,14 @@
 <?php
+
+$php7 = function (String $title):array{
+	return [
+	$title."\n",
+	"PHP on another leve",
+	"more performance",
+	"2x faster"
+	];
+};
+
+foreach ($php7("news in PHP 7") as $k) {
+	echo $k."\n";
+}
